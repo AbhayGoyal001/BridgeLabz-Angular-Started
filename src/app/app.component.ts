7368892876a5cea2de31ffe08c4fb8f11e576ddc
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-03-22 19:10:14 +0530

    [Abhay Goyal] Added DisplayLogo Files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
-})
-export class AppComponent {
-  title = 'HelloWorld';
-
-  ngOnInit() {
-    console.log('Hello from BridgeLabz');
-  }
-}
+ 
+ @Component({
+   selector: 'app-root',
+   imports: [RouterOutlet],
+   templateUrl: './app.component.html',
+   styleUrl: './app.component.scss',
+ })
+ export class AppComponent {
+   title = 'HelloWorld';
+   imgUrl = '../assets/BL_logo_square_jpg.jpg';
+ 
+   ngOnInit() {
+     console.log('Hello from BridgeLabz');}
+   }
