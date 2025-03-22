b7ad83e93c8e13562176e77ccd4d5575c6623a6a
AbhayGoyal001 <abhaygoyalscience@gmail.com>
2025-03-22 19:19:41 +0530

    [Abhay Goyal] Added NewTab with Home Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
- 
- @Component({
-   selector: 'app-root',
-   imports: [RouterOutlet],
-   templateUrl: './app.component.html',
-   styleUrl: './app.component.scss',
- })
- export class AppComponent {
-   title = 'HelloWorld';
-   imgUrl = '../assets/BL_logo_square_jpg.jpg';
- 
-   ngOnInit() {
-     console.log('Hello from BridgeLabz');}
-   }
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent {
+  title = 'HelloWorld';
+  imgUrl = '../assets/BL_logo_square_jpg.jpg';
+  url = 'https://www.bridgelabz.com';
+
+  ngOnInit() {
+    console.log('Hello from BridgeLabz');
+  }
+
+  onClick($event: any) {
+    console.log('Save button is clicked!', $event);
+    window.open(this.url, '_blank');
+  }
+}
